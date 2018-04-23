45601195c3a80cac7da463fa81c9cbc9c3dfd204
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using DevExpress.XtraScheduler;
+
+namespace SchedulerAppointmentPropertyChanged {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+
+            SchedulerStorage schedulerStorage = schedulerControl1.Storage;
+            Appointment apt = schedulerStorage.CreateAppointment(AppointmentType.Normal);
+            DateTime baseTime = DateTime.Today;
+
+            apt.Start = baseTime.AddHours(1);
+            apt.End = baseTime.AddHours(2);
+            apt.Subject = "Test";
+            apt.Location = "Office";
+            apt.Description = "Test procedure";
+
+            schedulerStorage.Appointments.Add(apt);
+
+            schedulerControl1.Start = baseTime;
+        }
+
+        private void schedulerStorage1_AppointmentChanging(object sender, PersistentObjectCancelEventArgs e) {
+            object oldLabel = ((DataRowView)((Appointment)e.Object).GetSourceObject((SchedulerStorageBase)sender)).Row["Label"];
+            object newLabel = ((Appointment)e.Object).LabelKey;
+
+            if (!newLabel.Equals(oldLabel))
+                MessageBox.Show("Label was changed!");
+        }
+    }
+}
